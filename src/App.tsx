fa9266034f9b984c679a731757883b8cfdc4c901
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2021-12-01 00:05:59 +0500

    Adding progress bar for file read

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
-import { Box, Flex } from "@chakra-ui/layout";
+import { Flex } from "@chakra-ui/layout";
 import { useEffect, useState } from "react";
 import Papa from "papaparse";
 import StepOne from "./components/StepOne";
+import { Progress } from "@chakra-ui/react";
 
 function App() {
   const [selectedFile, setSelectedFile] = useState<null | File>(null);
+  const [progressValue, setProgressValue] = useState(0);
+  const [excelFields, setExcelFields] = useState<string[]>([]);
 
   useEffect(() => {
     console.log("selectedFile: ", selectedFile);
     if (selectedFile) {
       Papa.parse(selectedFile, {
-        complete: (data: { data: Object; errors: Object; meta: Object }) =>
-          console.log(data),
+        complete: excelDataHandler,
         header: true,
       });
     }
   }, [selectedFile]);
 
+  const excelDataHandler = (data: Papa.ParseResult<unknown>) => {
+    console.log(data);
+    if (data?.meta?.fields?.length) {
+      setExcelFields(data?.meta?.fields);
+    }
+  };
+
   return (
     <Flex justify="center" align="center" w="100%" h="100vh" overflow="hidden">
       <StepOne setSelectedFile={setSelectedFile} />
+      {!!progressValue && (
+        <Progress value={progressValue} size="xs" colorScheme="green" />
+      )}
     </Flex>
   );
 }
